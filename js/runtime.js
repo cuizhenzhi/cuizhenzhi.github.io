15683d4d058a8d5c8f346a2478052d747de8c975
cuizhenzhi <2269752188a@gmail.com>
2023-07-31 16:34:34 +0800

    Site updated: 2023-07-31 16:34:33

--- js/runtime.js
+++ js/runtime.js
@@ -1,40 +1,6 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.css"><script src="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="https://cdn.jsdelivr.net/npm/meting/dist/Meting.min.js"></script>// setInterval(() => {
-//     let create_time = Math.round(new Date('2023-07-27 00:00:00').getTime() / 1000); //在此行修改建站时间
-//     let timestamp = Math.round((new Date().getTime()) / 1000);
-//     let second = timestamp - create_time;
-//     let time = new Array(0, 0, 0, 0, 0);
-//
-//     var nol = function(h){
-//         return h>9?h:'0'+h;
-//     }
-//     if (second >= 365 * 24 * 3600) {
-//         time[0] = parseInt(second / (365 * 24 * 3600));
-//         second %= 365 * 24 * 3600;
-//     }
-//     if (second >= 24 * 3600) {
-//         time[1] = parseInt(second / (24 * 3600));
-//         second %= 24 * 3600;
-//     }
-//     if (second >= 3600) {
-//         time[2] = nol(parseInt(second / 3600));
-//         second %= 3600;
-//     }
-//     if (second >= 60) {
-//         time[3] = nol(parseInt(second / 60));
-//         second %= 60;
-//     }
-//     if (second > 0) {
-//         time[4] = nol(second);
-//     }
-//     if ((Number(time[2])<3) && (Number(time[2])>8)){
-//         currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/奶茶店-营业中-6adea8?style=social&logo=bitbucket' title='努力进步中~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
-//     }
-//     else{
-//         currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/奶茶店-打烊了-6adea8?style=social&logo=bitbucket' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
-//     }
-//     document.getElementById("workboard").innerHTML = currentTimeHtml;
-// }, 1000);
-setInterval(() => {
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.css"><script src="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="https://cdn.jsdelivr.net/npm/meting/dist/Meting.min.js"></script>
+
+    setInterval(() => {
     let create_time = Math.round(new Date('2019-04-17 00:00:00').getTime() / 1000); //在此行修改建站时间
     let timestamp = Math.round((new Date().getTime()) / 1000);
     let second = timestamp - create_time;
